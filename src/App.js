97382ce1fb01ebaceeaabc7d3cd26b9e8fa733dd
Renispatel <renishpatel19@gmail.com>
2023-04-16 00:06:23 +0530

    add imdb project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Header from "./components/Hearder/Hearder";
+import Homepage from "./Pages/Homepage/Homepage";
+import MovieList from "./components/movieList/movieList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Routes>
+          <Route index element={<Homepage />}></Route>
+          <Route path="movie/:id" element={<h1>Movies List</h1>}></Route>
+          <Route path="movies/:type" element={<MovieList />}></Route>
+          <Route path="/*" element={<h1>Errodfgdgfdgfr Page</h1>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
